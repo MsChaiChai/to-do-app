18511816eb0beca81d975f4ac500b33c9696bf6b
Teodora <191014639+MsChaiChai@users.noreply.github.com>
2025-02-07 22:48:21 +0100

    Add group tasks by date functionality

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import TaskItem from './TaskItem';
+import formatDate from '../utils/date';
 
 
 function TaskList({tasks,onToggleTask,onDeleteTask}){
 
+
+    const groupedTasks=tasks.reduce((acc,task)=>{
+
+        const date=formatDate(task.id);
+
+        if(!acc[date]){
+            acc[date]=[];
+        }
+        acc[date].push(task);
+
+        return acc;
+    },{});
+
+    const sortedDates=Object.keys(groupedTasks).reverse();
+
     return(
-        <ul>
-            {tasks.map(task=>(
-                <li key={task.id}><TaskItem task={task} onToggleTask={onToggleTask} onDeleteTask={onDeleteTask}/></li>
-            ))}
-        </ul>
+        <div>
+            {sortedDates.map((date)=>{
+                return(
+                    <div key={date}>
+                        <h2>{date}</h2>
+                        {groupedTasks[date].map((task)=>{
+                            return(
+                                <TaskItem key={task.id} task={task} onToggleTask={onToggleTask} onDeleteTask={onDeleteTask}></TaskItem>
+                            );
+                        })}
+                    </div>)
+            })}
+        </div>
     )
+
 
 }
 
